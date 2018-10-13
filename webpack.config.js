00a9ba1258470522fa06cd87981323c40ac7e7a7
Brandon Porter <brandon.porter18@gmail.com>
2018-10-13 10:57:05 -0700

    Remove index.html and replace with default html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: [ '.ts', '.tsx', '.js', '.json' ]
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, use: 'ts-loader' }
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: 'ts-loader' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // Required
+            inject: false,
+            template: require('html-webpack-template'),
+            // Optional
+            title: 'Learn React',
+            appMountId: 'app'
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Learn React',
-      template: './src/index.html'
-    })
-  ]
 }
